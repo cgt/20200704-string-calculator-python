0fdbe0a31f7ebb56cece7bb0604110b676be69a9
Christoffer G. Thomsen <chris@cgt.name>
2020-07-04 17:33:39 +0200

    Return any single number

--- test_foo.py
+++ test_foo.py
@@ -1,6 +1,9 @@
 def test_empty_string_returns_zero():
     assert add("") == 0
 
+def test_add_0_returns_0():
+    assert add("0") == 0
+
 def test_add_4_returns_4():
     assert add("4") == 4
 
@@ -10,3 +13,4 @@ def add(numbers):
 
     if numbers == "4":
         return 4
+    return int(numbers)
