b4de4cdb841baf8e8bcade4d60ce69dd785713d2
Christoffer G. Thomsen <chris@cgt.name>
2020-07-04 17:53:50 +0200

    Sum up to three comma-separated numbers

--- test_foo.py
+++ test_foo.py
@@ -13,12 +13,17 @@ def test_add_1_and_2_returns_3():
 def test_add_3_and_5_returns_8():
     assert add("3,5") == 8
 
+def test_add_1_and_2_and_3_returns_6():
+    assert add("1,2,3") == 6
+
 def add(comma_separated_numbers):
     if comma_separated_numbers == "":
         return 0
     if "," in comma_separated_numbers:
         numbers = comma_separated_numbers.split(",")
         result = add(numbers[0]) + add(numbers[1])
+        if len(numbers) > 2:
+            result += add(numbers[2])
         return result
 
     return int(comma_separated_numbers)
