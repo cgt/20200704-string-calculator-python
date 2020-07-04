f2cf46aaf1292b2aae9603b0702895b5f0b26377
Christoffer G. Thomsen <chris@cgt.name>
2020-07-04 17:41:14 +0200

    Add 2 numbers returns their sum

--- test_foo.py
+++ test_foo.py
@@ -10,10 +10,14 @@ def test_add_4_returns_4():
 def test_add_1_and_2_returns_3():
     assert add("1,2") == 3
 
+def test_add_3_and_5_returns_8():
+    assert add("3,5") == 8
+
 def add(numbers):
     if numbers == "":
         return 0
     if "," in numbers:
-        return 3
+        x = numbers.split(",")
+        return add(x[0]) + add(x[1])
 
     return int(numbers)
