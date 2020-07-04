eeb14a3ee27af7216d8bc144246b51aff89e32e9
Christoffer G. Thomsen <chris@cgt.name>
2020-07-04 19:32:23 +0200

    Rename function

--- test_foo.py
+++ test_foo.py
@@ -29,7 +29,7 @@ def add(separated_numbers):
     if separated_numbers == "":
         return 0
     if separated_numbers.startswith("//"):
-        separated_numbers = normalize_separator(separated_numbers)
+        separated_numbers = normalize_custom_separator(separated_numbers)
 
     if "," in separated_numbers:
         return add_separated_numbers(separated_numbers, ",")
@@ -40,7 +40,7 @@ def add(separated_numbers):
     return int(separated_numbers)
 
 
-def normalize_separator(separated_numbers):
+def normalize_custom_separator(separated_numbers):
     parts = separated_numbers.split("\n", 1)
     separated_numbers = parts[1]
     prefix = parts[0]
