ef31e0194e239df117a71cee5d6b2dccc77ec1c6
Christoffer G. Thomsen <chris@cgt.name>
2020-07-04 17:37:54 +0200

    Add "1,2" returns 3

--- test_foo.py
+++ test_foo.py
@@ -7,8 +7,13 @@ def test_add_0_returns_0():
 def test_add_4_returns_4():
     assert add("4") == 4
 
+def test_add_1_and_2_returns_3():
+    assert add("1,2") == 3
+
 def add(numbers):
     if numbers == "":
         return 0
+    if "," in numbers:
+        return 3
 
     return int(numbers)
